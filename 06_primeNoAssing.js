bc97cdb82848a016c3a69b52da00e441740d5829
Prathmesh3080 <prathmeshpatil843@gmail.com>
2023-08-11 15:16:34 +0530

    JSON Assignment Complete
    10/8/2023

--- 06_primeNoAssing.js
+++ 06_primeNoAssing.js
@@ -1,8 +1,8 @@
 console.log(`=========== Assignment 2 ==========`);
 //`Check Given Number Prime Or Not` 11, 21
-let i,count=0;
+let i,count=0;//1,2
 function Prime(num) {
-    for(i=1; i<=num; i++)
+    for(i=1; i<num/2; i++)
     {
         if(num%i==0)
         {
@@ -20,3 +20,4 @@ function Prime(num) {
 }
 Prime(11);
 Prime(21);
+Prime(17);
---
+++ 13_jsonAssign.js
@@ -0,0 +1,25 @@
+console.log(`=========== Assignment 1 ==========`);
+const jsonstr = `{
+    "name": "Aleix Melon",
+    "id": "E00245",
+    "role":["Dev", "DBA"],
+    "age": 23,
+    "doj": "11-12-2019",
+    "married": false,
+    "address":{
+        "street": "32, Laham St.",
+        "city": "Innsburk",
+        "country": "Austria"
+    },
+    "refferred-by": "E0012"
+}`
+console.log(`------------- Step 1 ---------------------`);
+const convert1 = JSON.parse(jsonstr);
+console.log(typeof convert1);
+console.log(convert1);
+console.log(`------------- Step 2 ---------------------`);
+console.log(`Role is: ${convert1.role[0]}`);
+console.log(`------------- Step 3 ---------------------`);
+console.log(`Last Name: ${convert1.name.slice(6,11)}`);
+console.log(`------------- Step 4 ---------------------`);
+console.log(`DOJ Year: ${convert1.doj.slice(6,10)}`);
